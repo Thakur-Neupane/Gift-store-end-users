30ac473039154b7e45d84c8bfa8db6c8bcb10b39
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-09-22 11:54:03 +1000

    profile landing

--- src/pages/user/profileLanding/ProfileLanding.jsx
+++ src/pages/user/profileLanding/ProfileLanding.jsx
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 
 const ProfileLanding = () => {
   return (
-    <>
-      <Container className="page">
-        <Row className="align-items-center">
-          <Col md={6} className="info">
-            <h1>
-              RS <span>Store</span>
-            </h1>
-            <p className="fw-bolder">RS Store all for your gifts</p>
-            <Link to="/signIn">
-              <Button className="btn-hero">Login/Register</Button>
-            </Link>
-          </Col>
-          <Col md={6}>
-            <img src={main} alt="RS Gifting" className="img-fluid main-img" />
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container className="py-16 bg-gray-100">
+      <Row className="flex items-center">
+        <Col md={6} className="text-left">
+          <h1 className="text-4xl font-bold">
+            RS <span className="text-blue-500">Store</span>
+          </h1>
+          <p className="font-bold text-lg">
+            All your gifting needs in one place
+          </p>
+          <Link to="/signIn">
+            <Button className="mt-4 bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-200">
+              Login/Register
+            </Button>
+          </Link>
+        </Col>
+        <Col md={6}>
+          <img src={main} alt="RS Gifting" className="w-full h-auto" />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
