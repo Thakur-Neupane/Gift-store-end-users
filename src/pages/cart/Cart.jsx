e6ab668187cf62bde285a60caa35564aaf5e95f0
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-09-28 19:59:13 +1000

    cart verified

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -13,95 +13,94 @@ const Cart = () => {
   const user = useSelector((state) => state.userInfo.user);
 
   const getTotal = () => {
-    return cart.reduce((total, item) => total + item.count * item.price, 0);
+    return cart.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
   };
 
   const saveOrderToDb = async () => {
-    if (!user) {
+    if (user) {
+      const orderData = {
+        items: cart,
+        total: getTotal(),
+        userId: user._id,
+        title: cart.name,
+      };
+
+      try {
+        await dispatch(saveOrderAction(orderData));
+
+        // Navigate to checkout with the actual user ID
+        navigate(`/checkout/${user._id}`);
+      } catch (err) {
+        console.error("Error saving order:", err);
+      }
+    } else {
       navigate("/signIn", { state: { from: location.pathname } });
-      return;
-    }
-
-    const orderData = {
-      items: cart,
-      total: getTotal(),
-      userId: user._id,
-      title: cart.map((item) => item.name).join(", "),
-    };
-
-    try {
-      await dispatch(saveOrderAction(orderData));
-      navigate(`/checkout/${user._id}`);
-    } catch (err) {
-      console.error("Error saving order:", err);
     }
   };
 
   const handleProceedToCheckout = () => {
-    saveOrderToDb();
+    if (user) {
+      saveOrderToDb();
+    } else {
+      navigate("/signIn", { state: { from: location.pathname } });
+    }
   };
-
-  const renderCartItems = () => {
-    return cart.length > 0 ? (
-      <table className="table table-bordered">
-        <thead className="thead-light">
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col">Title</th>
-            <th scope="col">Price</th>
-            <th scope="col">Color</th>
-            <th scope="col">Count</th>
-            <th scope="col">Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((item) => (
-            <ProductCardInCheckout key={item._id} p={item} />
-          ))}
-        </tbody>
-      </table>
-    ) : (
-      <p>
-        No products in cart. <Link to="/shop">Continue Shopping.</Link>
-      </p>
-    );
-  };
-
-  const renderOrderSummary = () => (
-    <>
-      <h4>Order Summary</h4>
-      <hr />
-      <p>Products</p>
-      {cart.map((item, index) => (
-        <div key={index}>
-          <p>
-            {item.name} x {item.count} = ${item.price * item.count}
-          </p>
-        </div>
-      ))}
-      <hr />
-      Total: <b>${getTotal()}</b>
-      <hr />
-      <button
-        onClick={handleProceedToCheckout}
-        className="btn btn-sm btn-primary mt-2"
-        disabled={!cart.length}
-      >
-        {user ? "Proceed to Checkout" : "Login to Checkout"}
-      </button>
-    </>
-  );
 
   return (
     <div className="container-fluid pt-2">
       <div className="row">
         <div className="col-md-8">
           <h4>
-            Cart / {cart.length} Product{cart.length !== 1 ? "s" : ""}
+            Cart / {cart.length} Product{cart.length > 1 ? "s" : ""}
           </h4>
-          {renderCartItems()}
+          {!cart.length ? (
+            <p>
+              No products in cart. <Link to="/shop">Continue Shopping.</Link>
+            </p>
+          ) : (
+            <table className="table table-bordered">
+              <thead className="thead-light">
+                <tr>
+                  <th scope="col">Image</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Color</th>
+                  <th scope="col">Count</th>
+                  <th scope="col">Remove</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((p) => (
+                  <ProductCardInCheckout key={p._id} p={p} />
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
-        <div className="col-md-4">{renderOrderSummary()}</div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((c, i) => (
+            <div key={i}>
+              <p>
+                {c.name} x {c.count} = ${c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Total: <b>${getTotal()}</b>
+          <hr />
+          <button
+            onClick={handleProceedToCheckout}
+            className="btn btn-sm btn-primary mt-2"
+            disabled={!cart.length}
+          >
+            {user ? "Proceed to Checkout" : "Login to Checkout"}
+          </button>
+        </div>
       </div>
     </div>
   );
